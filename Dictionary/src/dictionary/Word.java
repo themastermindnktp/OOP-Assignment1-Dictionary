87e3445b7cb3b5bd53f6de76852ada77cf194a5c
themastermindnktp <36890561+themastermindnktp@users.noreply.github.com>
2018-10-17 14:33:17 +0700

    Add comments for Word.java

--- Dictionary/src/dictionary/Word.java
+++ Dictionary/src/dictionary/Word.java
@@ -1,7 +1,13 @@
+/* Authors: Do Hoang Khanh & Nguyen Thanh Dat */
 package dictionary;
 
 public class Word
 {
+    /*
+        Word class is used to contain the information of
+        an English word and its Vietnamese definition
+    */
+
     private String engWord;     //English word
     private String definition;  //Vietnamese definition of word
 
